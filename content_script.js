86b6c8da11eb628a06ab67e65d20498bdede1aa9
GirkovArpa <GirkovArpa@protonmail.com>
2020-09-04 13:30:13 -0500

    :fire: Delete console.log calls.

--- content_script.js
+++ content_script.js
@@ -1,11 +1,9 @@
 'use strict';
 
 async function mainLoop() {
-  console.log('YT Keep Watching Extension - mainLoop()');
   let iteration = 0;
   while (true) {
     iteration++;
-    console.log('YT Keep Watching Extension - loop iteration #' + iteration);
     const notNowButton = document.querySelector('[aria-label="Not Now"]');
     notNowButton && notNowButton.click();
     await new Promise(resolve => setTimeout(resolve, 250));
