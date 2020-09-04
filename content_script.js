b323a33a7a7bcf2f5718749123c0e3a6fae3422e
GirkovArpa <GirkovArpa@protonmail.com>
2020-09-04 13:28:32 -0500

    :bug: Wrap body of mainLoop inside actual loop.

--- content_script.js
+++ content_script.js
@@ -1,9 +1,15 @@
 'use strict';
 
-const mainLoop = async function () {
-  const notNowButton = document.querySelector('[aria-label="Not Now"]');
-  notNowButton && notNowButton.click();
-  await new Promise(resolve => setTimeout(resolve, 250));
+async function mainLoop() {
+  console.log('YT Keep Watching Extension - mainLoop()');
+  let iteration = 0;
+  while (true) {
+    iteration++;
+    console.log('YT Keep Watching Extension - loop iteration #' + iteration);
+    const notNowButton = document.querySelector('[aria-label="Not Now"]');
+    notNowButton && notNowButton.click();
+    await new Promise(resolve => setTimeout(resolve, 250));
+  }
 }
 
 mainLoop();
