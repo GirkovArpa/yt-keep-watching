85ca26dd72441de0614afeb039b249e157202ec1
GirkovArpa <GirkovArpa@protonmail.com>
2020-09-04 13:30:30 -0500

    :fire: Delete iteration counter.

--- content_script.js
+++ content_script.js
@@ -1,9 +1,7 @@
 'use strict';
 
 async function mainLoop() {
-  let iteration = 0;
   while (true) {
-    iteration++;
     const notNowButton = document.querySelector('[aria-label="Not Now"]');
     notNowButton && notNowButton.click();
     await new Promise(resolve => setTimeout(resolve, 250));
