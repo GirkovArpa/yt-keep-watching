094dc80001e8dc2ab95b2fad4f556bcb799dad25
GirkovArpa <GirkovArpa@protonmail.com>
2020-09-03 21:13:55 -0500

    :sparkles: Check for "Not Now" button every 250ms and click it if it exists.

--- content_script.js
+++ content_script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const mainLoop = async function () {
+  const notNowButton = document.querySelector('[aria-label="Not Now"]');
+  notNowButton && notNowButton.click();
+  await new Promise(resolve => setTimeout(resolve, 250));
+}
+
+mainLoop();
