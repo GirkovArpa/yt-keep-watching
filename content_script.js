df8ec0083768e30e42e78e2197b37dd64451dea8
GirkovArpa <GirkovArpa@protonmail.com>
2020-09-04 19:28:28 -0500

    :sparkles: Bypass "Video Paused. Continue Watching?" overlay by clicking "Yes."

--- content_script.js
+++ content_script.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const $ = document.querySelector.bind(document);
+
 async function mainLoop() {
   while (true) {
-    const notNowButton = document.querySelector('[aria-label="Not Now"]');
+    const continueWatchingButton = $('[aria-label="Yes"]');
+    continueWatchingButton && continueWatchingButton.click();
+    const notNowButton = $('[aria-label="Not Now"]');
     notNowButton && notNowButton.click();
     await new Promise(resolve => setTimeout(resolve, 250));
   }
